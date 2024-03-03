3d9fbbf28a840dbf91c41fec5db7d6a7fa348106
duclqDev99 <duclq300499@gmail.com>
2024-03-03 10:18:36 +0700

    tạo function ordercreate

--- backend/src/models/OrderModel.js
+++ backend/src/models/OrderModel.js
@@ -12,17 +12,46 @@ const findById = (id) => {
   return db(tableName).where('id', id).first();
 };
 
+// const create = async (order) => {
+//     const trx = await db.transaction(); 
+//     try {
+//         const ids = await db(tableName).insert(order);
+//         await trx.commit();
+//         return ids;
+//     } catch (error) {
+//         await trx.rollback();
+//         throw error;
+//     }
+// };
+
 const create = async (order) => {
-    const trx = await db.transaction(); 
+    console.log('order',order);
     try {
-        const ids = await db(tableName).insert(order);
-        await trx.commit();
-        return ids;
+      const result = await db.transaction(async trx => {
+        const [orderId] = await trx('orders').insert({
+          id_user: order.id_user,
+          total_amount: order.total_amount,
+          status: 1 
+        }, 'id');
+  
+        const orderDetails = order.products.map(product => ({
+          id_order: orderId,
+          id_product: product.id,
+          quantity: product.quantity,
+          sub_total: 0 
+        }));
+        console.log('orderDetails',orderDetails);
+  
+        await trx('order_detail').insert(orderDetails);
+  
+        return orderId; 
+      });
+  
+      console.log(`Đơn hàng đã được tạo thành công với ID: ${result}`);
     } catch (error) {
-        await trx.rollback();
-        throw error;
+      console.error(`Lỗi khi tạo đơn hàng: ${error.message}`);
     }
-};
+  }
 
 const update = async (id, order) => {
     const trx = await db.transaction(); 
